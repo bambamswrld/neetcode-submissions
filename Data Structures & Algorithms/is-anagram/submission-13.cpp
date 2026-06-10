8b67e70f333c21780bec46a27e849a9e3c1bba7d
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-06-10 16:19:05 -0700

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int count[26] = {0};
+        auto sLen = s.size();
+        auto tLen = t.size();
+        auto cLen = 26;
+
+        if(sLen != tLen)
+        {
+            return false;
+        }
+
+        for(int i = 0; i < sLen; i++)
+        {
+            count[s.at(i) - 'a']++;
+            count[t.at(i) - 'a']--;
+        }
+
+        for(int i = 0; i < cLen; i++)
+        {
+            if(count[i] != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
