75199577433d18f7c7ea0fac4afbb3da32351e4d
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-06-13 13:22:35 -0700

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        std::unordered_map<std::string, vector<string>> map;
+
+        for(const auto& s : strs)
+        {
+            vector<int> count(26,0);            
+        
+            for(char c : s)
+            {
+                count[c - 'a']++;
+            }
+
+            string key = "";
+            
+            for(int i = 0; i < 26; i++)
+            {
+                key += ',' + to_string(count[i]);
+            }
+            map[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for(const auto& pair : map)
+        {
+            result.push_back(pair.second);
+        }      
+        return result;
+    }
+};
