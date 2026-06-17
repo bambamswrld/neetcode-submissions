b65b61b3f084a095db048f99591c4f71c93b58ad
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-06-16 19:07:52 -0700

    Add: valid-perfect-square - submission-3

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long begin = 1;
+        long long end = num;
+
+        while(begin <= end)
+        {
+            long long mid = begin + (end - begin) / 2;
+            long long sqr = mid * mid;
+            if(sqr < num)
+            {
+                begin = mid + 1;
+            }
+            else if(sqr > num)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
