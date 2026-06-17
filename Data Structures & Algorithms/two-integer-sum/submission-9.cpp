1b505ab7b98c48ff29188b1f64304970ea72ac32
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-06-17 16:18:20 -0700

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.cpp
@@ -0,0 +1,14 @@
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            int difference = target - nums[i];
+            if (map.count(difference)) {
+                return {map.at(difference), i};
+            }
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
