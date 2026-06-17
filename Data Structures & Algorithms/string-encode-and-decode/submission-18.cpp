5381f897c23f5dfc2312ba87cd0e2eeda3f5f32c
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-06-17 15:35:31 -0700

    Add: string-encode-and-decode - submission-18

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-18.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        std::string res = "";
+        
+        for(auto& s : strs)
+        {
+            res += std::to_string(s.size()) + '#' + s;
+        }
+        
+        return res;
+    }
+
+    vector<string> decode(string s) {
+        std::vector<std::string> res;
+        int sLen = s.size();
+        int i = 0;
+        while(i != sLen)
+        {
+            int j = i;
+            while(s[j] != '#')
+            {
+                j++;
+            }
+            int length = std::stoi(s.substr(i, j));
+            res.push_back(s.substr(j + 1, length));
+            i = j + 1 + length;
+        }
+        return res;
+    }
+};
