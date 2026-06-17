04e9891ce53badc6705f35442bd51ea24fcb9aa9
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-06-16 17:26:30 -0700

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        const int len = nums.size();
+        std::vector<int> pre(len);
+        std::vector<int> post(len);
+        int i = 0;
+        
+        pre[0] = 1;
+        post[len - 1] = 1;
+        
+        for(i = 1; i < len; i++)
+        {
+            pre[i] = pre[i - 1] * nums[i - 1];
+        }
+
+        for(i = len - 2; i >= 0; i--)
+        {
+            post[i] = post[i + 1] * nums[i + 1];
+        }
+
+        std::vector<int> res(len);        
+        
+        for(i = 0; i < len; i++)
+        {
+            res[i] = pre[i] * post[i];
+        }
+
+        return res;
+    }
+};
