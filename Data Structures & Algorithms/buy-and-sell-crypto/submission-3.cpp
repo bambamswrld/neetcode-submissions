32d136d15934c4156982e34b7ca69d2d8a99f755
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-06-22 01:22:40 -0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int buy = prices[0];
+        int profit = 0;
+
+        if(n == 1) { return 0; }
+
+        for(int i = 1; i < n; i++)
+        {
+            int currPrice = prices[i];
+            if(currPrice < buy)
+            {
+                buy = currPrice;
+            }
+
+            if((currPrice - buy) > profit)
+            {
+                profit = currPrice - buy;
+            }
+        }
+
+        return profit;
+    }
+};
