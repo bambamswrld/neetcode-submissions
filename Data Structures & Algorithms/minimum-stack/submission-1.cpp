22447d6e7b62791591482f12ef4c9c46d6cfe88e
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-06-22 00:55:28 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+class MinStack {
+private:
+    std::stack<int> stk;
+    std::stack<int> tmp;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        stk.push(val);
+        val = std::min(val, tmp.empty() ? val : tmp.top());
+        tmp.push(val);
+    }
+    
+    void pop() {
+        stk.pop();
+        tmp.pop();
+    }
+    
+    int top() {
+        return stk.top();
+    }
+    
+    int getMin() {
+        return tmp.top();
+    }
+};
