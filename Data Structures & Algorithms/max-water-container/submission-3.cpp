d5284acffaaf67d756278818d9804ef340ace75c
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-06-24 20:49:31 -0700

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int h = heights.size();
+        int l = 0;
+        int r = h - 1;
+        int res = 0;
+        int area = 0;
+
+        while(l < r)
+        {
+            area = (r - l) * std::min(heights[l], heights[r]);
+            res = std::max(area, res);
+
+            if(heights[l] <= heights[r])
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+        return res;
+    }
+};
