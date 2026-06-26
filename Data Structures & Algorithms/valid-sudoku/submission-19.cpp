42eaadb2dbc3368b7e8d3c4ef0787f8b74550c8b
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-06-25 23:03:05 -0700

    Add: valid-sudoku - submission-19

---
+++ Data Structures & Algorithms/valid-sudoku/submission-19.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(auto row = 0; row < 9; row++)
+        {
+            std::unordered_set<char> seen;
+            for(int i = 0; i < 9; i++)
+            {
+                auto c = board[row][i];
+                if(c == '.')
+                {
+                    continue;
+                }
+                if(seen.count(c))
+                {
+                    return false;
+                }
+                seen.insert(c);
+            }
+        }
+
+        for(auto col = 0; col < 9; col++)
+        {
+            std::unordered_set<char> seen;
+            for(auto i = 0; i < 9; i++)
+            {
+                auto c = board[i][col];
+                if(c == '.')
+                {
+                    continue;
+                }
+                if(seen.count(c))
+                {
+                    return false;
+                }
+                seen.insert(c);
+            }
+        }
+
+        for(int square = 0; square < 9; square++)
+        {
+            std::unordered_set<char> seen;
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    int row = (square / 3) * 3 + i;
+                    int col = (square % 3) * 3 + j;
+                    auto c = board[row][col];
+                    if(c == '.')
+                    {
+                        continue;
+                    }
+                    if(seen.count(c))
+                    {
+                        return false;
+                    }
+                    seen.insert(c);
+                }
+            }
+        }
+
+        return true;
+    }
+};
