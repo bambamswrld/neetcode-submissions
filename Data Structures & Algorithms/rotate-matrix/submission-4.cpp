fa99b6f63fffa548785910629e5125c793b19ef4
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-06-26 16:27:31 -0700

    Add: rotate-matrix - submission-4

---
+++ Data Structures & Algorithms/rotate-matrix/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int l = 0;
+        int r = m - 1;
+
+        while(l < r)
+        {
+            for(int i = 0; i < r - l; i++)
+            {
+                int top = l;
+                int bottom = r;
+                int topLeft = matrix[top][l + i];
+
+                matrix[top][l + i] = matrix[bottom - i][l];
+                matrix[bottom - i][l] = matrix[bottom][r - i];
+                matrix[bottom][r - i] = matrix[top + i][r];
+                matrix[top + i][r] = topLeft;
+            }
+            l++;
+            r--;
+        }
+    }
+};
