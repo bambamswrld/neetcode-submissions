9c71396a6880fd61c53a3d2466b24ecfc80dff60
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-06-27 15:03:56 -0700

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+        int cML = 0;
+        int cMR = 0;
+        int res = 0;
+
+        while(l < r)
+        {
+            if(height[l] > cML)
+            {
+                cML = height[l];
+            }
+            res += cML - height[l];
+
+            if(height[r] > cMR)
+            {
+                cMR = height[r];
+            }
+            res += cMR - height[r];
+
+            if(height[l] < height[r])
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+        return res;
+    }
+};
