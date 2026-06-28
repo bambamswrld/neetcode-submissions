8012205271b4359eacb28f241171418b1f25f3db
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-06-27 18:18:48 -0700

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std::stack<char> stk;
+
+        for(int i = 0; i < s.size(); i++)
+        {            
+            if(s[i] == '[' || s[i] == '{' || s[i] == '(')
+            {
+                stk.push(s[i]);
+            }
+            else if(s[i] == ']')
+            {
+                if(stk.empty() || stk.top() != '[')
+                {
+                    return false;
+                }
+                stk.pop();
+            }
+            else if (s[i] == '}')
+            {
+                if(stk.empty() || stk.top() != '{')
+                {
+                    return false;
+                }
+                stk.pop();
+            }
+            else if(s[i] == ')')
+            {
+                if(stk.empty() || stk.top() != '(')
+                {
+                    return false;
+                }
+                stk.pop();
+            }
+        }
+        return stk.empty();
+    }
+};
