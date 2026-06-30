e380e57cd0e0a7e5a061a95d9506496e7a81eb46
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-06-29 23:57:34 -0700

    Add: length-of-last-word - submission-4

---
+++ Data Structures & Algorithms/length-of-last-word/submission-4.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size(), r = n - 1, count = 0;
+
+        while(!isalnum(s[r])) { r--; }
+
+        while(isalnum(s[r]))  { count++; r--; }
+
+        return count;
+    }
+};
