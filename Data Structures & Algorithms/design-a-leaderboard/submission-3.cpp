480fc5d61b51443171b549b33aab73e59ccac8e4
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-06-30 15:53:17 -0700

    Add: design-a-leaderboard - submission-3

---
+++ Data Structures & Algorithms/design-a-leaderboard/submission-3.cpp
@@ -0,0 +1,64 @@
+class Leaderboard {
+public:
+    std::unordered_map<int, int> scores;
+    std::map<int, int, greater<int>> sortedScores;
+
+    Leaderboard() = default;
+    
+    void addScore(int playerId, int score) {
+        if(!scores.contains(playerId))
+        {
+            scores[playerId] = score;
+            sortedScores[score]++;
+        }
+        else
+        {
+            int preScore = scores[playerId];
+            sortedScores[preScore]--;
+            
+            if(sortedScores[preScore] == 0)
+            {
+                sortedScores.erase(preScore);
+            }
+
+            int newScore = preScore + score;
+            scores[playerId] = newScore;
+            sortedScores[newScore]++;
+        }
+    }
+    
+    int top(int K) {
+        int count = 0, sum = 0;
+
+        for(auto& [key, times] : sortedScores)
+        {
+            for(int i = 0; i < times; i++)
+            {
+                sum += key;
+                count++;
+                if(count == K) { break; }
+            }
+            if(count == K) { break; }
+        }
+        return sum;
+    }
+    
+    void reset(int playerId) {
+        int preScore = scores[playerId];
+        sortedScores[preScore]--;
+
+        if(sortedScores[preScore] == 0)
+        {
+            sortedScores.erase(preScore);
+        }
+        scores.erase(playerId);
+    }
+};
+
+/**
+ * Your Leaderboard object will be instantiated and called as such:
+ * Leaderboard* obj = new Leaderboard();
+ * obj->addScore(playerId,score);
+ * int param_2 = obj->top(K);
+ * obj->reset(playerId);
+ */
