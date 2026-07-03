a62f2f77f70d59817ed55353b8de3807494c3854
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-07-03 16:49:00 -0700

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int res = 0;
+        std::unordered_map<char, int> count;
+
+        int l = 0, maxf = 0;
+
+        for(int r = 0; r < s.size(); r++)
+        {
+            count[s[r]]++;
+            maxf = std::max(maxf, count[s[r]]);
+
+            while((r - l + 1) - maxf > k)
+            {
+                count[s[l]]--;
+                l++;
+            }
+            res = std::max(res, r - l + 1);
+        }
+        
+        return res;
+    }
+};
