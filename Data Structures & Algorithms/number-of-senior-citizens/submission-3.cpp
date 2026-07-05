69dcf8642664a37a506265eff2278d444cdccdbf
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-07-04 21:22:07 -0700

    Add: number-of-senior-citizens - submission-3

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-3.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& strings) {
+        int res = 0;
+        for(auto& s : strings)
+        {
+            int age = std::stoi(s.substr(11, 2));
+            if(age > 60) { res++; }
+        }
+        return res;
+    }
+};
