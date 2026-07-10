46c42b18c9f3bd458028a0d2574ecf125e5234e6
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-07-09 22:32:40 -0700

    Add: delete-n-nodes-after-m-nodes-of-a-linked-list - submission-8

---
+++ Data Structures & Algorithms/delete-n-nodes-after-m-nodes-of-a-linked-list/submission-8.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+   public:
+    ListNode* deleteNodes(ListNode* head, int m, int n) {
+        ListNode* curr = head;
+
+        while (curr) {
+            for (int i = 1; i < m && curr; i++) {
+                curr = curr->next;
+            }
+
+            if (!curr) {
+                break;
+            }
+
+            ListNode* scout = curr;
+
+            for (int i = 0; i < n && scout->next; i++) {
+                scout = scout->next;
+            }
+
+            curr->next = scout->next;
+            curr = curr->next;
+        }
+
+        return head;
+    }
+};
