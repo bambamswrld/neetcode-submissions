82980f5762dfeefa82c5ac6dab3501e9c433d72d
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-07-13 20:13:27 -0700

    Add: path-sum - submission-6

---
+++ Data Structures & Algorithms/path-sum/submission-6.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    bool dfs(TreeNode* root, int currSum, int targetSum)
+    {
+        if(!root)
+            return false;
+        
+        currSum += root->val;
+
+        if(!root->left && !root->right)
+            return currSum == targetSum;
+        
+        return dfs(root->left, currSum, targetSum) || dfs(root->right, currSum, targetSum);
+    }
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        return dfs(root, 0, targetSum);
+    }
+};
