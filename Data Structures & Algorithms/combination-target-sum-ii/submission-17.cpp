424087b1c67c6d1d6bf085694ff2030d87650345
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-07-20 17:59:49 -0700

    Add: combination-target-sum-ii - submission-17

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-17.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    void dfs(vector<int>& candidates, vector<int>& subset, int i, int target)
+    {
+        if(target == 0)
+        {
+            res.push_back(subset);
+            return;
+        }
+
+        if(target < 0)
+            return;
+
+        for(int j = i; j < candidates.size(); j++)
+        {
+            if(j > i && candidates[j] == candidates[j - 1])
+                continue;
+            subset.push_back(candidates[j]);
+            dfs(candidates, subset, j + 1, target - candidates[j]);
+            subset.pop_back();
+        }
+    }
+public:
+    std::vector<std::vector<int>> res;
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        std::vector<int> subset;
+        std::sort(candidates.begin(), candidates.end());
+        dfs(candidates, subset, 0, target);
+        return res;
+    }
+};
