155788919a90ffaff0f4c63e2e68ac1051d18e64
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-07-20 17:18:25 -0700

    Add: combination-target-sum - submission-11

---
+++ Data Structures & Algorithms/combination-target-sum/submission-11.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    void dfs(vector<int>& nums, vector<int>& subset, vector<vector<int>>& res, int i, int target)
+    {
+        if(target == 0)
+        {
+            res.push_back(subset);
+            return;
+        }            
+
+        if(target < 0)
+            return;
+
+        for(int j = i; j < nums.size(); j++)
+        {
+            subset.push_back(nums[j]);
+            dfs(nums, subset, res, j, target - nums[j]);
+            subset.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        std::vector<std::vector<int>> res;
+        std::vector<int> subset;
+        sort(nums.begin(), nums.end());
+        dfs(nums, subset, res, 0, target);
+        return res;
+    }
+};
