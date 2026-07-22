0bbca1ab6015eef77b3d9778bf29c245b1d610e4
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-07-22 01:20:55 -0700

    Add: search-for-word - submission-8

---
+++ Data Structures & Algorithms/search-for-word/submission-8.cpp
@@ -0,0 +1,42 @@
+class Solution {
+private:
+    bool dfs(vector<vector<char>>& board, string word, int r, int c, int i)
+    {
+        if(i == word.length())
+            return true;
+
+        if(r < 0 || c < 0 || r >= ROWS || c >= COLS || board[r][c] != word[i] || visited[r][c])
+        {
+            return false;
+        }
+
+        visited[r][c] = true;
+        bool res = dfs(board, word, r + 1, c, i + 1) ||
+                   dfs(board, word, r - 1, c, i + 1) ||
+                   dfs(board, word, r, c + 1, i + 1) ||
+                   dfs(board, word, r, c - 1, i + 1);
+
+        visited[r][c] = false;
+        return res;
+    }
+public:
+    int ROWS;
+    int COLS;
+    std::vector<std::vector<bool>> visited;
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        ROWS = board.size();
+        COLS = board[0].size();
+        visited = std::vector<std::vector<bool>>(ROWS, std::vector<bool>(COLS, false));
+
+        for(int r = 0; r < ROWS; r++)
+        {
+            for(int c = 0; c < COLS; c++)
+            {
+                if(dfs(board, word, r, c, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
