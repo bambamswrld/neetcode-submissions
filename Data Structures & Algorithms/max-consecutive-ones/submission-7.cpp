18ee20888b0ebcd6f6e48ff1fa10c080bf182fd8
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-07-28 00:05:28 -0700

    Add: max-consecutive-ones - submission-7

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-7.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        auto count = 0;
+        auto sum = 0;
+
+        int l = 0;
+        int r = 0;
+
+        while(r < nums.size())
+        {
+
+            if(nums[r] == 0)
+            {
+                l = r + 1;
+                r++;
+                sum = 0;
+            }
+            else
+            {
+                sum++;
+                r++;
+            }
+            count = std::max(sum, count);
+        }
+
+        return count;
+    }
+};
