cb3a48afab66cdbfdc8a60da118f7051f510f955
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-07-31 13:51:24 -0700

    Add: special-array-i - submission-2

---
+++ Data Structures & Algorithms/special-array-i/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        if (nums.size() == 1)
+            return true;
+            
+        std::vector<std::pair<int, int>> res;
+
+        for (auto i = 0; i < nums.size() - 1; i++)
+        {
+            res.push_back({nums[i], nums[i + 1]});
+        }
+
+        for (auto& r : res)
+        {
+            if((r.first % 2 == 0 && r.second % 2 != 0) || (r.first % 2 != 0 && r.second % 2 == 0))
+                continue;
+            else
+                return false;
+        }
+
+        return true;
+    }
+};
