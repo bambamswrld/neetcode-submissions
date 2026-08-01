ffade1271b85481dff85aa4f86cc81c63ed7780f
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-08-01 16:47:35 -0700

    Add: find-median-in-a-data-stream - submission-10

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-10.cpp
@@ -0,0 +1,46 @@
+class MedianFinder {
+private:
+    std::priority_queue<int> small;
+    std::priority_queue<int, std::vector<int>, greater<int>> large;      
+public:
+    MedianFinder() {}
+    
+    void addNum(int num) {
+        small.push(num);
+        
+        if(!large.empty() && small.top() > large.top())
+        {
+            large.push(small.top());
+            small.pop();
+        }
+        
+        int ss = small.size();
+        int ls = large.size();
+        
+        if(ss > ls + 1)
+        {
+            large.push(small.top());
+            small.pop();
+        }
+
+        if(ls > ss + 1)
+        {
+            small.push(large.top());
+            large.pop();
+        }
+    }
+    
+    double findMedian() {
+        int ss = small.size();
+        int ls = large.size();
+
+        if(ss > ls)
+            return small.top();
+        else if(ls > ss)
+            return large.top();
+        else
+        {
+            return (double)(small.top() + large.top()) / 2;
+        }
+    }
+};
