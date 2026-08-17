e21130f8993a096012af4dbfa803c566c68a49c0
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-08-17 02:48:35 -0400

    Add: three-integer-sum - submission-22

---
+++ Data Structures & Algorithms/three-integer-sum/submission-22.cpp
@@ -0,0 +1,49 @@
+class Solution {
+// {-4, -1, -1, 0, 1, 2}
+//.  ^   ^            ^              
+//.  i   l            r
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        std::vector<std::vector<int>> res;
+        std::sort(nums.begin(), nums.end());
+        if (nums[0] > 0)
+            return {};
+
+        for (int i = 0; i < nums.size() - 2; i++)
+        {
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int l = i + 1;
+            int r = nums.size() - 1;
+
+            while (l < r)
+            {
+                auto sum = nums[i] + nums[l] + nums[r];
+                if (sum > 0)
+                {
+                    r--;
+                }
+                else if (sum < 0)
+                {
+                    l++;
+                }
+                else
+                {
+                    res.push_back({nums[i], nums[l], nums[r]});
+                    l++;
+                    r--;
+
+                    while(l < r && nums[l] == nums[l - 1])
+                    {
+                        l++;
+                    }
+
+                    while(l < r && nums[r] == nums[r + 1])
+                    {
+                        r--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
