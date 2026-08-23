69f124d1a4f42bc8d0616d701cd5918e1da2151a
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-08-23 18:12:25 -0400

    Add: max-area-of-island - submission-64

---
+++ Data Structures & Algorithms/max-area-of-island/submission-64.cpp
@@ -0,0 +1,56 @@
+class Solution {
+int rowDirs[4] = {-1, 1, 0, 0};
+int colDirs[4] = {0, 0, 1, -1};
+private:
+    int ROWS, COLS;
+    std::queue<std::pair<int, int>> q;
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        ROWS = grid.size();
+        COLS = grid[0].size();
+        int area = 0;
+
+        for (int r = 0; r < ROWS; r++)
+        {
+            for (int c = 0; c < COLS; c++)
+            {
+                if (grid[r][c] == 1)
+                {
+
+                    area = std::max(area, bfs(grid, r, c));
+                }
+            }
+        }
+        return area;
+    }
+
+    int bfs(vector<vector<int>>& grid, int r, int c)
+    {
+        int steps = 1;
+
+        grid[r][c] = 0;
+        q.push({r, c});
+        
+        while (!q.empty())
+        {
+            auto curr = q.front();
+            q.pop();
+            int row = curr.first;
+            int col = curr.second;
+
+            for (int i = 0; i < 4; i++)
+            {
+                int nr = row + rowDirs[i];
+                int nc = col + colDirs[i];
+
+                if (nr > -1 && nc > -1 && nr < ROWS && nc < COLS && grid[nr][nc] == 1)
+                {
+                    q.push({nr, nc});
+                    grid[nr][nc] = 0;
+                    steps++;
+                }
+            }
+        }
+        return steps;
+    }
+};
