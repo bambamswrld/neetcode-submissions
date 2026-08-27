5d4226f2077df25e6ad6543437410bcf5185a135
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-08-27 01:10:09 -0400

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        std::vector<std::vector<int>> adj(numCourses);
+        std::vector<int> indegree(numCourses, 0);
+        std::queue<int> q;
+        std::vector<int> res;
+        for (auto& p : prerequisites)
+        {
+            adj[p[1]].push_back(p[0]);
+            indegree[p[0]]++;
+        }
+
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        int processed = 0;
+
+        while (!q.empty())
+        {
+            auto course = q.front();
+            q.pop();
+            res.push_back(course);
+            processed++;
+
+            for (auto next : adj[course])
+            {
+                if (--indegree[next] == 0)
+                    q.push(next);
+            }
+        }
+
+        if (processed != numCourses)
+            return {};
+        
+        return res;
+    }
+};
