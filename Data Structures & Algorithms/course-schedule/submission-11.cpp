f334a3f2f88a217e57c72bafd526953220ec1bfe
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-08-27 00:52:58 -0400

    Add: course-schedule - submission-11

---
+++ Data Structures & Algorithms/course-schedule/submission-11.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        std::vector<std::vector<int>> adj(numCourses);
+        std::vector<int> inDegree(numCourses, 0);
+        std::queue<int> q;
+
+        for (auto& p : prerequisites)
+        {
+            adj[p[1]].push_back(p[0]);
+            inDegree[p[0]]++;
+        }
+
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (inDegree[i] == 0)
+                q.push(i);
+        }
+
+        int processed = 0;
+
+        while (!q.empty())
+        {
+            auto course = q.front();
+            q.pop();
+
+            processed++;
+
+            for (auto next : adj[course])
+            {
+                if (--inDegree[next] == 0)
+                    q.push(next);
+            }
+        }
+
+        return processed == numCourses;
+    }
+};
