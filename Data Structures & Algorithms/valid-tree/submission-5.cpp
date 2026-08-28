7fcadb7f7bb43aa65892be1217365e136b121e7f
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-08-27 22:48:56 -0400

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if (edges.size() > n)
+            return false;
+        
+        std::vector<std::vector<int>> adj(n);
+        std::unordered_set<int> visit;
+        std::queue<std::pair<int,int>> q;
+
+        for (auto& edge : edges)
+        {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+
+        q.push({0, -1});
+        visit.insert(0);
+        
+        while (!q.empty())
+        {
+            auto [node, parent] = q.front();
+            q.pop();
+
+            for (auto nei : adj[node])
+            {
+                if (nei == parent)
+                    continue;
+
+                if (visit.count(nei))
+                    return false;
+
+                visit.insert(nei);
+                q.push({nei, node});
+            }
+        }
+        return visit.size() == n;
+    }
+};
