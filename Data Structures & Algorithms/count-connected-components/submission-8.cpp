5f972fc6073a56428a6a7fe5ed456fd102c754d7
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-09-06 16:49:26 -0400

    Add: count-connected-components - submission-8

---
+++ Data Structures & Algorithms/count-connected-components/submission-8.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        std::vector<std::vector<int>> adj(n);
+        std::unordered_set<int> visit;
+        std::queue<std::pair<int,int>> q;
+        if (n == 0)
+            return 0;
+
+        int count = 0;
+
+        for (auto& edge : edges)
+        {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }   
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (visit.count(i))
+                continue;
+
+            count++;
+            q.push({i, -1});
+            visit.insert(i);
+            
+            while (!q.empty())
+            {
+                auto [node, parent] = q.front();
+                q.pop();
+
+                for (auto next : adj[node])
+                {
+                    if (next == parent || visit.count(next))
+                        continue;
+                    
+                    visit.insert(next);
+                    q.push({next, node});
+                }   
+            }
+        }
+
+        return count;
+    }
+};
