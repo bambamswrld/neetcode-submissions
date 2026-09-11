689e154a8cd29bb961815846f78d3489a23a0917
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-09-11 18:02:45 -0400

    Add: is-anagram - submission-31

---
+++ Data Structures & Algorithms/is-anagram/submission-31.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        std::array<int, 26> count = {0};
+        int sLen = s.size();
+        int tLen = t.size();
+        
+        if (sLen != tLen)
+            return false;
+
+        for (int i = 0; i < sLen; i++)
+        {
+            char c = s[i];
+            char d = t[i];
+            count[c - 'a']++;
+            count[d - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (count[i] != 0)
+                return false;
+        }
+
+        return true;
+    }
+};
