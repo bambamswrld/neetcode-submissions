2c941b7266aa5e43784ab8fc26a02fac609dd7c3
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-09-23 20:09:33 -0400

    Add: redundant-connection - submission-10

---
+++ Data Structures & Algorithms/redundant-connection/submission-10.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        auto n = edges.size();
+        std::vector<std::vector<int>> adj(n + 1);
+        std::vector<int> indegree(n + 1, 0);
+        std::queue<int> q;
+
+        for (auto& edge : edges)
+        {
+            adj[edge[0]].emplace_back(edge[1]);
+            adj[edge[1]].emplace_back(edge[0]);
+            indegree[edge[0]]++;
+            indegree[edge[1]]++;
+        }
+
+        for (auto i = 1; i <= n; i++)
+        {
+            if (indegree[i] == 1)
+                q.push(i);
+        }
+
+        while (!q.empty())
+        {
+            auto node = q.front();
+            q.pop();
+            indegree[node]--;
+
+            for (auto& next : adj[node])
+            {
+                if (--indegree[next] == 1)
+                    q.push(next);
+            }
+        }
+
+        for (auto i = n - 1; i >= 0; i--)
+        {
+            auto u = edges[i][0], v = edges[i][1];
+
+            if (indegree[u] == 2 && indegree[v] == 2)
+                return {u, v};
+        }
+
+        return {};
+    }
+};
