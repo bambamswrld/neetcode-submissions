448c186543445ed4d7a51ae392d67247ee999420
Sam Mikhak <161412718+bambamswrld@users.noreply.github.com>
2026-09-24 17:48:15 -0400

    Add: word-ladder - submission-3

---
+++ Data Structures & Algorithms/word-ladder/submission-3.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        std::unordered_set<std::string> wordSet(wordList.begin(), wordList.end());
+        if (!wordSet.count(endWord))
+            return 0;
+
+        std::unordered_map<std::string, std::vector<std::string>> patterns;
+        std::unordered_set<std::string> visited;
+        std::queue<std::string> q;
+
+        for (auto& word : wordList)
+        {
+            for (auto i = 0; i < word.size(); i++)
+            {
+                std::string p = word;
+                p[i] = '*';
+                patterns[p].push_back(word);
+            }
+        }
+
+        q.push(beginWord);
+        visited.insert(beginWord);
+        int steps = 1;
+
+        while (!q.empty())
+        {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++)
+            {
+                auto word = q.front();
+                q.pop();
+
+                if (word == endWord)
+                    return steps;
+
+                for (int j = 0; j < word.size(); j++)
+                {
+                    std::string p = word;
+
+                    p[j] = '*';
+                    
+                    for (auto& next : patterns[p])
+                    {
+                        if (!visited.count(next))
+                        {
+                            visited.insert(next);
+                            q.push(next);
+                        }
+                    }   
+                }
+            }
+            steps++;
+        }
+        return 0;
+    }
+};
